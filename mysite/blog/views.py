418170b5fea0f601eb430866d073576884135de0
jaeyoon <jaeyoon040324@gmail.com>
2025-02-19 03:21:54 +0900

    2/19 3:21

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
